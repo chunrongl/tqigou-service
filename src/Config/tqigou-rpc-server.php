5c9ee36430ed245d199b435f61b436edef44c14a
chunrongl <chunrongl12@outlook.com>
2019-12-23 09:23:23 +0800

    鉴权+记录来源日志

--- src/Config/tqigou-rpc-server.php
+++ src/Config/tqigou-rpc-server.php
@@ -1,4 +1,6 @@
 <?php
 return array(
-    'uris'=>json_decode(env('TQIGOU_SERVICE_LISTEN_URIS','["tcp:\/\/127.0.0.1:1122"]'))
+    'uris'         => env('TQIGOU_SERVICE_LISTEN_URIS', '127.0.0.1:1122'),
+    'secret'       => env('TQIGOU_SERVICE_SECRET', ''),
+    'auth_version' => 1.1
 );
--- src/Services/Socket.php
+++ src/Services/Socket.php
@@ -8,25 +8,32 @@ use Hprose\Swoole\Server;
 
 class Socket
 {
-    public function register(){
+    private $clients;
+
+    public function register()
+    {
         $uri = $this->getRealListen();
 
-
+        /** @var \Hprose\Socket\Server $server */
         $server = new Server($uri, SWOOLE_PROCESS);
 
         $server->setErrorTypes(E_ALL);
         $server->setDebugEnabled(false);
 
+        $server->addInvokeHandler(array($this, "middleHandle"));
 
         $server->onSendError = function ($error, \stdClass $context) {
-            \Log::error($error);
-            throw new BusinessException($error->getMessage(),$error->getCode());
+            //            \Log::error($error);
+            $message = "Exception:" . $error->getMessage() . " in " . $error->getFile() . ":" . $error->getLine() . " called client:" . $this->tqigou_client_user;
+            \Log::error($message);
+            throw new BusinessException($error->getMessage(), $error->getCode());
         };
 
         return $server;
     }
 
-    private function getRealListen(){
+    private function getRealListen()
+    {
         $uri = config('tqigou-rpc-server.uris');
 
         if (empty($uri)) {
@@ -36,6 +43,42 @@ class Socket
         $realUri = "tcp://" . ltrim($uri, "tcp://");
 
         return $realUri;
+    }
+
+    public function middleHandle($name, array &$args, \stdClass $context, \Closure $next)
+    {
+
+        $this->validateAuth($args, $context);
+
+        $result = $next($name, $args, $context);
+        return $result;
+    }
+
+    private function validateAuth(&$args)
+    {
+        $this->clients = null;
+        $clientMemo = end($args);
+        if (isset($clientMemo['tqigou_client_version']) && $clientMemo['tqigou_client_version'] >= config('tqigou-rpc-server.auth_version')) {
+
+            if ($clientMemo['tqigou_client_secret'] !== config('tqigou-rpc-server.secret')) {
+                throw new InvalidConfigException("Illegal calls Tqigou-server", 500);
+            }
+
+            array_pop($args);
+
+            $this->clients = $clientMemo;
+        }
+
+    }
+
+    public function __get($name)
+    {
+        // TODO: Implement __get() method.
+        if (isset($this->clients[$name])) {
+            return $this->clients[$name];
+        }
+
+        return null;
     }
 
 }
